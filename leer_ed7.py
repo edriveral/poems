839ccddd6d1b96c7c7187eba0b5dd86737755df6
Edgar Rivera <edriveral@yahoo.es>
2023-03-08 00:00:37 -0500

    Un nuevo poema y su respectivo archivo .py

---
+++ leer_ed7.py
@@ -0,0 +1,21 @@
+﻿import utiles as ut
+
+ut.tit(' Leyendo un archivo ', clr=ut.Ci, clrB=ut.NeB, simb='&')
+
+# Creamos la instancia objeto "archivo" con la clase open
+archivo = open("canto a la luna.txt", 'r')
+
+# Usamos el método read() para guardar el contenido en "contenido"
+contenido = archivo.read()
+
+# Cerramos el objeto archivo.
+archivo.close()
+
+# Obtenemos el nombre del archivo con name.
+nombre = archivo.name
+
+print("El contenido de", nombre.upper(), "es:")
+print()
+ut.prcl(contenido, clr=ut.Am)
+
+input()
